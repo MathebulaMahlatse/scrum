bf65b57b241196c8dced04aedc824565d268975e
William Mathebula <william.mathebula@dariel.co.za>
2017-01-11 23:03:50 +0200

    dist folder with updated files

---
+++ test/spec/services/storiesServiceSpec.js
@@ -0,0 +1,12 @@
+/*
+fdescribe('Unit Test for StoriesService', function() {
+    beforeEach(module('prodAngularApp.storiesService'));
+
+    var storiesService, httpBackEnd;
+    beforeEach(inject(function(StoriesService, $httpBackend) {
+        storiesService = StoriesService;
+        httpBackEnd = $httpBackend;
+
+        httpBackEnd.
+    }));
+});*/
