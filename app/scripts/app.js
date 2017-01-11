91734e2350b5b54e05cbb56490026adb270f5b77
William Mathebula <william.mathebula@dariel.co.za>
2017-01-11 16:55:29 +0200

    auto deploy and remove exclamation marks in the url

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,7 +17,7 @@ angular
     'ngSanitize',
     'ngTouch'
   ])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
@@ -30,4 +30,7 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+
+      $locationProvider.hashPrefix('');
+
   });
