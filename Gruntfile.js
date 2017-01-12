7faf1e703c1cee5e448c73a04f5338b231b1d911
William Mathebula <william.mathebula@dariel.co.za>
2017-01-12 18:31:26 +0200

    update grunt file and style team page

--- Gruntfile.js
+++ Gruntfile.js
@@ -386,7 +386,17 @@ module.exports = function (grunt) {
         }, {
           expand: true,
           cwd: '.',
+          src: 'bower_components/*/*.*',
+          dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
+          dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: '<%= yeoman.app %>',
+          src: 'mock/*.*',
           dest: '<%= yeoman.dist %>'
         }]
       },
