c418fb5dae90f2b14104403898b285574216b639
William Mathebula <william.mathebula@dariel.co.za>
2017-01-13 19:46:15 +0200

    add tests for stories

--- app/scripts/services/storiesServices.js
+++ app/scripts/services/storiesServices.js
@@ -2,11 +2,43 @@ var module = angular.module('scrum.storiesService', []);
 
 module.factory('StoriesService', ['$http', function ($http) {
     var _team = {};
+    var _cachedScrumData = undefined;
+
+    function generateStoryId(stories) {
+        var lastAddedStory = _.last(stories);
+        return lastAddedStory.storyId + 1;
+    }
+
     return {
         getStoriesAssignedToTeams: function () {
-            return $http.get('/mock/scrumData.json').then(function (response) {
-                return response.data;
-            })
+            if(_.isUndefined(_cachedScrumData)) {
+                return $http.get('/mock/scrumData.json').then(function (response) {
+                    _cachedScrumData = response.data;
+                    return response.data;
+                })
+            } else {
+                return _cachedScrumData;
+            }
+
+        },
+
+        addStoriesAssignedToTeam: function (storyAssociatedWithATeam) {
+            var story = storyAssociatedWithATeam.story;
+            _.forEach(_cachedScrumData.teams, function (team) {
+                if(team.teamId == storyAssociatedWithATeam.teamId) {
+                    team.stories.push({
+                        storyId: generateStoryId(team.stories),
+                        description: story.description,
+                        estimation: story.estimation,
+                        owner: story.owner,
+                        status: story.status
+                    });
+
+                    return team;
+                }
+
+                return team;
+            });
         },
         storeSelectedTeam: function (selectedTeam) {
             _team = selectedTeam;
--- test/spec/services/storiesServiceSpec.js
+++ test/spec/services/storiesServiceSpec.js
@@ -1,12 +1,96 @@
-/*
-fdescribe('Unit Test for StoriesService', function() {
-    beforeEach(module('prodAngularApp.storiesService'));
+describe('Unit Test for StoriesService', function() {
+    beforeEach(angular.mock.module('scrum.storiesService'));
 
-    var storiesService, httpBackEnd;
-    beforeEach(inject(function(StoriesService, $httpBackend) {
-        storiesService = StoriesService;
+    var storiesService, httpBackEnd, actual;
+
+    var mockTeamData = {
+        teams: [
+            {
+                teamId: 1,
+                stories: [
+                    {
+                        storyId: 1,
+                        status: ''
+                    }
+                ]
+            },
+            {
+                teamId: 2,
+                stories: [
+                    {
+                        storyId: 1,
+                        status: ''
+                    }
+                ]
+            }
+        ]
+    };
+
+    beforeEach(inject(function($httpBackend, StoriesService) {
         httpBackEnd = $httpBackend;
+        storiesService = StoriesService;
 
-        httpBackEnd.
+        httpBackEnd.expectGET('/mock/scrumData.json').respond(200, mockTeamData);
+
+        storiesService.getStoriesAssignedToTeams().then(function (result) {
+            actual = result;
+        });
+        httpBackEnd.flush();
+
     }));
-});*/
+
+
+    describe('getStoriesAssignedToTeams', function () {
+        it('get stories assigned to team', function () {
+            expect(storiesService.getStoriesAssignedToTeams()).toEqual(mockTeamData)
+        });
+    });
+
+    describe('addStoriesAssignedToTeam', function () {
+        it('should store stories', function () {
+            var story = {
+                teamId: 1,
+                story: {
+                    storyId: 1,
+                    description: 'testing',
+                    estimation: 'large',
+                    status: 'backlog',
+                    owner: 'William, developer'
+                }
+            };
+
+            storiesService.addStoriesAssignedToTeam(story);
+
+            expect(storiesService.getStoriesAssignedToTeams()).toEqual({
+                teams: [
+                    {
+                        teamId: 1,
+                        stories: [
+                            {
+                                storyId: 1,
+                                status: ''
+                            },
+                            {
+                                storyId: 2,
+                                description: 'testing',
+                                estimation: 'large',
+                                status: 'backlog',
+                                owner: 'William, developer'
+                            }
+                        ]
+                    },
+                    {
+                        teamId: 2,
+                        stories: [
+                            {
+                                storyId: 1,
+                                status: ''
+                            }
+                        ]
+                    }
+                ]
+            });
+
+        });
+    });
+});
