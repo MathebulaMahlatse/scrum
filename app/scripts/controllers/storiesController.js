14252c6d855201b8bd973fbd81088baba2bb7b02
William Mathebula <william.mathebula@dariel.co.za>
2017-01-13 11:04:51 +0200

    styling stories page

--- app/scripts/controllers/storiesController.js
+++ app/scripts/controllers/storiesController.js
@@ -3,13 +3,19 @@ var module = angular.module('scrum.storiesController',
         'scrum.storiesService'
     ]);
 
-module.controller('StoriesController', ['$scope', 'StoriesService', function ($scope, StoriesService) {
-    filterSelectedTeam(StoriesService.retrieveSelectedTeam());
+module.controller('StoriesController', ['$scope', '$location', 'StoriesService', function ($scope, $location, StoriesService) {
+    var selectedTeam = StoriesService.retrieveSelectedTeam();
+
+    if(selectedTeam && !selectedTeam.teamName) {
+        $location.path('/');
+    }
+    $scope.teamName = selectedTeam && selectedTeam.teamName;
+    filterSelectedTeam(selectedTeam);
 
     function filterSelectedTeam(selectedTeam) {
         $scope.backlogTasks = _.filter(selectedTeam.stories, {status: 'backlog'});
         $scope.todoTasks = _.filter(selectedTeam.stories, {status: 'todo'});
-        $scope.inProgressTasks = _.filter(selectedTeam.stories, {status: 'inProgress'});
+        $scope.inProgressTasks = _.filter(selectedTeam.stories, {status: 'in progress'});
         $scope.completedTasks = _.filter(selectedTeam.stories, {status: 'done'});
     }
 }]);
