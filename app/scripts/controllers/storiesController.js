997c3f9c59910e10a529b5227dabaeeab469dbfe
William Mathebula <william.mathebula@dariel.co.za>
2017-01-14 18:54:07 +0200

    refactor stories controller

--- app/scripts/controllers/storiesController.js
+++ app/scripts/controllers/storiesController.js
@@ -10,9 +10,8 @@ module.controller('StoriesController', ['$scope', '$location', 'StoriesService',
     if(selectedTeam && !selectedTeam.teamName) {
         $location.path('/');
     }
-    $scope.teamName = selectedTeam && selectedTeam.teamName;
-    filterSelectedTeam(selectedTeam);
 
+    $scope.teamName = selectedTeam && selectedTeam.teamName;
 
     $scope.addOrEditStory = function (story) {
         ModalService.showModal({
@@ -28,31 +27,39 @@ module.controller('StoriesController', ['$scope', '$location', 'StoriesService',
         });
     };
 
-    $scope.storiesViewModel = [
-        {
-            header: 'Backlog',
-            stories: $scope.backlogTasks
-        },
-        {
-            header: 'Todo',
-            stories: $scope.todoTasks
-        },
-        {
-            header: 'In progress',
-            stories: $scope.inProgressTasks
-        },
-        {
-            header: 'Done',
-            stories: $scope.completedTasks
+    refreshViewModel(filterSelectedTeamForStories(selectedTeam.stories));
+
+    function filterSelectedTeamForStories(stories) {
+        return {
+            backlogTasks: filterStoriesBasedOnStatus(stories, 'backlog'),
+            todoTasks: filterStoriesBasedOnStatus(stories, 'todo'),
+            inProgressTasks: filterStoriesBasedOnStatus(stories, 'in progress'),
+            completedTasks: filterStoriesBasedOnStatus(stories, 'done')
         }
-    ];
+    }
 
+    function filterStoriesBasedOnStatus(stories, status) {
+        return _.filter(stories, {  status: status});
+    }
 
-
-    function filterSelectedTeam(selectedTeam) {
-        $scope.backlogTasks = _.filter(selectedTeam.stories, {status: 'backlog'});
-        $scope.todoTasks = _.filter(selectedTeam.stories, {status: 'todo'});
-        $scope.inProgressTasks = _.filter(selectedTeam.stories, {status: 'in progress'});
-        $scope.completedTasks = _.filter(selectedTeam.stories, {status: 'done'});
+    function refreshViewModel(stories) {
+        $scope.storiesViewModel = [
+            {
+                header: 'Backlog',
+                stories: stories.backlogTasks
+            },
+            {
+                header: 'Todo',
+                stories: stories.todoTasks
+            },
+            {
+                header: 'In progress',
+                stories: stories.inProgressTasks
+            },
+            {
+                header: 'Done',
+                stories: stories.completedTasks
+            }
+        ];
     }
 }]);
